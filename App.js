8598c34eb50c8a35c09db7a21033ace32090b96f
Mirka Heikkila <mirkanms@hotmail.com>
2021-11-08 18:42:42 +0200

    'toimii'

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, StyleSheet, Text, View, Button, TextInput, FlatList, StatusBar, Image } from 'react-native';
 
 export default function App() {
+  const [keyword, setKeyword] = useState('');
+  const [repositories, setRepositories] = useState([]);
+ 
+  const getRepositories = () => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${keyword}`)
+    .then(response => response.json())
+    .then(responseData => setRepositories(responseData.meals))
+    .catch(error => { 
+        Alert.alert('Error', error); 
+    });    
+  }
+  
+  const listSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "80%",
+          backgroundColor: "#CED0CE",
+          marginLeft: "10%"
+        }}
+      />
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden={true} />
+      <FlatList 
+        style={{marginLeft : "5%"}}
+        keyExtractor={(item, index) => index.toString}
+        renderItem={({ item }) =>
+          <View>
+            <Image source={{uri: item.strMealThumb}} style={styles.images}></Image>
+            <Text style={{fontSize: 18, fontWeight: "bold"}}>{item.strMeal}</Text>
+          </View>}
+        data={repositories} 
+        ItemSeparatorComponent={listSeparator} /> 
+      <TextInput style={{fontSize: 18, width: 200}} placeholder='keyword' 
+        onChangeText={text => setKeyword(text)} />
+      <Button title="Find" onPress={getRepositories} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ container: {
+  flex: 1,
+  backgroundColor: '#fff',
+  alignItems: 'center',
+  justifyContent: 'center',
+ },
+ images: {
+   width: '50%',
+   height: 100,
+ }
 });
